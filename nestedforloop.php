8a1848b269709918f583b2ba5a5193f238c25223
SidharthMuthu0 <B00149989@mytudublin.ie>
2024-10-01 19:15:44 +0100

    Loop in five

---
+++ nestedforloop.php
@@ -0,0 +1,18 @@
+<?php
+echo "<table border='1' cellpadding='5'>";
+
+for ($i=1;$i<=15;$i++) {
+    echo "<tr>";
+
+
+    for ($j=1;$j<=20;$j++) {
+        echo "<td>$i,$j</td>";
+    }
+
+    echo "</tr>";
+}
+echo "</table";
+
+?>
+
+
